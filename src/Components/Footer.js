519d0e4fa824430de22ddf57e478f81d875cf6e7
Anjuman Raj <ayusmann23@gmail.com>
2022-08-28 15:50:40 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             <footer className="p-4 bg-[#fbefbc] rounded-lg shadow md:flex md:items-center md:justify-center md:p-6 dark:bg-gray-800">
                 <span className="text-sm text-center  sm:text-center">© 2022 <NavLink to="/" className="hover:underline text-gray-900 font-semibold ">Anime Inc<span className='font-black text-red-600' >.</span>™</NavLink>.Designed & Developed  by - 
                 <AttentionSeeker effect='tada' className='inline' > 
-                <a href="https:/anjuman.tech/" className='text-red-600 font-sans font-bold ' >  Anjuman Raj </a>
+                <a href="https://www.anjuman.tech/" className='text-red-600 font-sans font-bold ' >  Anjuman Raj </a>
                 </AttentionSeeker>
                 </span>
 
